7508d697509654f4fd20c6e5dae27cf77cad1a8a
Justin Joyce <justinwjoyce@gmail.com>
2017-07-26 09:48:24 -0500

    finish chef tests"

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -53,5 +53,12 @@ class ChefTest < ActiveSupport::TestCase
 		assert_not duplicate_chef.valid?
 	end
 
+	test "email should be lower case before hitting db" do
+		mixed_email = "JohN@ExampLe.com"
+		@chef.email = mixed_email
+		@chef.save
+		assert_equal mixed_email.downcase, @chef.reload.email
+	end
+
 
 end
