bd5df7ed86622db4773028a5fab33602c366afa8
Justin Joyce <justinwjoyce@gmail.com>
2017-07-28 11:41:44 -0500

    add delete recipes and test

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+	def setuo
+		@chef = Chef.create!(chefname: "justin", email: "justin@exampel.com")
+		@recipe = Recipe.create(name: "vegetable saute", description: "gerat vegetable oil", chef: @chef)
+	end
+
+	test "succesfully delete a recipe" do
+		get recipe_path(@recipe)
+		assert_template 'recipe/show'
+		assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+		assert_difference 'Recipe.count', -1 do
+			delete recipe_path(@recipe)
+		end
+		assert_redirected_to recipe_path
+		assert_not flash.empty?
+	end
+end
